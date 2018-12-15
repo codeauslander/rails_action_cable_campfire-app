7e73af387dce43a7b5e34ea19407a25867251714
codeauslander <cesar.gusto.catano@gmail.com>
2018-12-14 23:57:41 -0600

    practice websockets rails action cable

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,30 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    // Called when there's incoming data on the websocket for this channel
+    alert(data['message']);
+  },
+
+  speak: function(message) {
+    return this.perform('speak',{message: message});
+  },
+});
+
+(function() {
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13) {
+      App.room.speak(event.target.value);
+      event.target.value = '';
+      return event.preventDefault();
+    }
+  });
+
+}).call(this);
+
